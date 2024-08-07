677d044d68a4d691453f0447e59c0c13302672c6
HR Wu <5631010+heiruwu@users.noreply.github.com>
2024-08-08 07:00:23 +0800

    feat(llama2-7b-chat): add llama2-7b-chat implementation (#8)
    Because
    
    - Showcase llama2-7b-chat model implementation that is Instill Model
    compatible
    
    This commit
    
    - add llama2-7b-chat **implementation**
    
    Local test
    ```
    llama2-7b-chat git:(heiru/INS-5713) instill run admin/llama2 -g -i '{"prompt": "why cat so cute"}'
    2024-08-08 06:54:10,747.747 INFO     [Instill] Starting model image...
    2024-08-08 06:54:21,141.141 INFO     [Instill] Deploying model...
    2024-08-08 06:54:44,213.213 INFO     [Instill] Running inference...
    2024-08-07 15:54:48,807.807 INFO     [Instill] Outputs:
    [{'data': {'choices': [{'created': 1723071288,
                            'finish-reason': 'length',
                            'index': 0,
                            'message': 'Cats are considered cute for a variety of '
                                       'reasons. Here are some possible '
                                       'explanations:\n'
                                       '\n'
                                       '1. Evolutionary advantages: Cuteness is a '
                                       'form of infant'}]}}]
    2024-08-08 06:54:51,630.630 INFO     [Instill] Done
    ```

---
+++ llama2-7b-chat/model.py
@@ -0,0 +1,84 @@
+# pylint: skip-file
+import os
+
+# TORCH_GPU_MEMORY_FRACTION = 0.95  # Target memory ~= 15G on 16G card
+# TORCH_GPU_MEMORY_FRACTION = 0.38  # Target memory ~= 15G on 40G card
+TORCH_GPU_DEVICE_ID = 0
+os.environ["CUDA_VISIBLE_DEVICES"] = f"{TORCH_GPU_DEVICE_ID}"
+
+import time
+import torch
+import transformers
+from transformers import LlamaTokenizer
+
+from conversation import Conversation, conv_templates, SeparatorStyle
+from instill.helpers.ray_config import instill_deployment, InstillDeployable
+from instill.helpers import (
+    parse_task_chat_to_chat_input,
+    construct_task_chat_output,
+)
+
+
+@instill_deployment
+class Llama2Chat:
+    def __init__(self):
+        self.tokenizer = LlamaTokenizer.from_pretrained("Llama-2-7b-chat-hf")
+        self.pipeline = transformers.pipeline(
+            "text-generation",
+            model="Llama-2-7b-chat-hf",
+            torch_dtype=torch.float16,
+            device_map="auto",
+            # prefer_safe=True,
+        )
+
+    # async def ModelInfer(self, request: ModelInferRequest) -> ModelInferResponse:
+    async def __call__(self, request):
+        conversation_inputs = await parse_task_chat_to_chat_input(request=request)
+
+        if len(conversation_inputs) > 1:
+            raise Exception("this model does not support batch input")
+
+        conv = self.pipeline.tokenizer.apply_chat_template(
+            conversation_inputs[0].messages, tokenize=False, add_generation_prompt=True
+        )
+
+        terminators = [
+            self.pipeline.tokenizer.eos_token_id,
+            self.pipeline.tokenizer.convert_tokens_to_ids("<|eot_id|>"),
+        ]
+
+        sequences = self.pipeline(
+            conv,
+            do_sample=True,
+            top_p=conversation_inputs[0].top_p,
+            temperature=conversation_inputs[0].temperature,
+            num_return_sequences=conversation_inputs[0].n,
+            eos_token_id=terminators,
+            max_length=conversation_inputs[0].max_tokens,
+        )
+
+        text_outputs = []
+        for seq in sequences:
+            generated_text = (
+                seq["generated_text"].split("[/INST]")[-1].strip().encode("utf-8")
+            )
+            text_outputs.append(generated_text)
+
+        finish_reasons = [[]]
+        indexes = [[]]
+        created = [[]]
+        for i in range(len(text_outputs)):
+            finish_reasons[0].append("length")
+            indexes[0].append(i)
+            created[0].append(int(time.time()))
+
+        return construct_task_chat_output(
+            request=request,
+            finish_reasons=finish_reasons,
+            indexes=indexes,
+            created_timestamps=created,
+            messages=[text_outputs],
+        )
+
+
+entrypoint = InstillDeployable(Llama2Chat).get_deployment_handle()
