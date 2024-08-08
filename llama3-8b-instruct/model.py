2cc69d6f84c6348de1db19d95c03bb906f3ac9f5
HR Wu <5631010+heiruwu@users.noreply.github.com>
2024-08-08 15:09:08 +0800

    feat(llama3-8b-instruct): add llama3-8b-instruct implementation (#9)
    Because
    
    - Showcase llama3-8b-instruct implementation that is Instill Model
    compatible
    
    This commit
    
    - add llama3-8b-instruct implementation
    
    Local test
    ```
    llama3-8b-instruct git:(heiru/INS-5714) ✗ instill run admin/llama3 -g -i '{"prompt": "why cat so cute"}'
    2024-08-08 14:58:35,797.797 INFO     [Instill] Starting model image...
    2024-08-08 14:58:46,213.213 INFO     [Instill] Deploying model...
    2024-08-08 14:59:18,937.937 INFO     [Instill] Running inference...
    2024-08-07 23:59:23,404.404 INFO     [Instill] Outputs:
    [{'data': {'choices': [{'created': 1723100363,
                            'finish-reason': 'length',
                            'index': 0,
                            'message': 'The eternal question!\n'
                                       '\n'
                                       'There are many reasons why cats are often '
                                       "considered cute, and it's a combination of "
                                       'their physical characteristics, behavior, '
                                       'and evolution. Here are some possible '
                                       'explanations:\n'
                                       '\n'
                                       "1. **Evolutionary advantages**: Cats' "
                                       'physical features, such'}]}}]
    2024-08-08 14:59:26,950.950 INFO     [Instill] Done
    ```

---
+++ llama3-8b-instruct/model.py
@@ -0,0 +1,81 @@
+# pylint: skip-file
+import time
+import torch
+import transformers
+
+import numpy as np
+
+from instill.helpers.ray_config import instill_deployment, InstillDeployable
+from instill.helpers import (
+    parse_task_chat_to_chat_input,
+    construct_task_chat_output,
+)
+
+
+@instill_deployment
+class Llama3Instruct:
+    def __init__(self):
+        self.pipeline = transformers.pipeline(
+            "text-generation",
+            model="Meta-Llama-3-8B-Instruct",
+            model_kwargs={"torch_dtype": torch.bfloat16},
+            device="cuda",
+        )
+
+    async def __call__(self, request):
+        chat_inputs = await parse_task_chat_to_chat_input(request=request)
+
+        terminators = [
+            self.pipeline.tokenizer.eos_token_id,
+            self.pipeline.tokenizer.convert_tokens_to_ids("<|eot_id|>"),
+        ]
+
+        finish_reasons = []
+        indexes = []
+        created = []
+        messages = []
+        for inp in chat_inputs:
+            conv = self.pipeline.tokenizer.apply_chat_template(
+                inp.messages,
+                tokenize=False,
+                add_generation_prompt=True,
+            )
+
+            sequences = self.pipeline(
+                conv,
+                do_sample=True,
+                temperature=inp.temperature,
+                top_p=inp.top_p,
+                num_return_sequences=inp.n,
+                eos_token_id=terminators,
+                max_new_tokens=inp.max_tokens,
+            )
+
+            finish_reasons_per_seq = []
+            indexes_per_seq = []
+            created_per_seq = []
+            messages_per_seq = []
+            for i, seq in enumerate(sequences):
+                generated_text = (
+                    seq["generated_text"].split("<|start_header_id|>assistant<|end_header_id|>")[-1].strip().encode("utf-8")
+                )
+                messages_per_seq.append(generated_text)
+                finish_reasons_per_seq.append("length")
+                indexes_per_seq.append(i)
+                created_per_seq.append(int(time.time()))
+
+            finish_reasons.append(finish_reasons_per_seq)
+            indexes.append(indexes_per_seq)
+            created.append(created_per_seq)
+            messages.append(messages_per_seq)
+
+        return construct_task_chat_output(
+            request=request,
+            finish_reasons=finish_reasons,
+            indexes=indexes,
+            created_timestamps=created,
+            messages=messages,
+        )
+
+
+entrypoint = InstillDeployable(Llama3Instruct).get_deployment_handle()
