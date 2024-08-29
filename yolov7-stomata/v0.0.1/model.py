ff6fd87cb787a2dd634b5e232f60f318b1507a7d
HR Wu <5631010+heiruwu@users.noreply.github.com>
2024-08-29 15:31:07 +0800

    feat(jina-clip): add jina-clip implementation (#16)
    Because
    
    - add jina-clip implementation for image embedding task
    - add version folder to support `instill-core` compatibility metrix
    chart
    
    This commit
    
    - add jina-clip implementation
    - add version folder

--- yolov7-stomata/v0.0.1/model.py
+++ yolov7-stomata/v0.0.1/model.py
@@ -1,31 +1,71 @@
+from itertools import groupby
+
 import torch
+import cv2
 import numpy as np
 
-from itertools import groupby
 from utils.general import non_max_suppression, scale_coords, check_img_size
 from utils.augmentations import letterbox
 from utils.segment.general import process_mask, scale_masks
 from utils.torch_utils import select_device
 from models.common import DetectMultiBackend
+from instill.helpers.const import DataType
+from instill.helpers.ray_io import serialize_byte_tensor, deserialize_bytes_tensor
 from instill.helpers.ray_config import instill_deployment, InstillDeployable
 from instill.helpers import (
-    parse_task_instance_segmentation_to_vision_input,
-    construct_task_instance_segmentation_output,
+    construct_infer_response,
+    construct_metadata_response,
+    Metadata,
 )
 
 
 @instill_deployment
 class StomataYolov7:
     def __init__(self):
-        # self.device = select_device("cuda:0")
+        self.label = ["stomata", "outer_line"]
         self.device = select_device("cpu")
         self.model = DetectMultiBackend(
-            "model.pt", device=self.device, dnn=False, data=None, fp16=False
+            "model-multilabel.pt", device=self.device, dnn=False, data=None, fp16=False
         )
 
         self.image_size = check_img_size(640, s=self.model.stride)
 
         self.model.warmup()  # warmup
+
+    def ModelMetadata(self, req):
+        resp = construct_metadata_response(
+            req=req,
+            inputs=[
+                Metadata(
+                    name="input",
+                    datatype=str(DataType.TYPE_STRING.name),
+                    shape=[1],
+                ),
+            ],
+            outputs=[
+                Metadata(
+                    name="rles",
+                    datatype=str(DataType.TYPE_STRING.name),
+                    shape=[-1],
+                ),
+                Metadata(
+                    name="boxes",
+                    datatype=str(DataType.TYPE_FP32),
+                    shape=[-1, 4],
+                ),
+                Metadata(
+                    name="labels",
+                    datatype=str(DataType.TYPE_STRING.name),
+                    shape=[-1],
+                ),
+                Metadata(
+                    name="scores",
+                    datatype=str(DataType.TYPE_FP32),
+                    shape=[-1],
+                ),
+            ],
+        )
+        return resp
 
     def rle_encode(self, binary_mask):
         r"""
@@ -86,18 +126,17 @@ class StomataYolov7:
 
         return rles, ret_boxes, ret_labels, ret_scores
 
-    async def __call__(self, request):
-        vision_inputs = await parse_task_instance_segmentation_to_vision_input(
-            request=request
-        )
+    async def __call__(self, req):
+        b_tensors = req.raw_input_contents[0]
+
+        input_tensors = deserialize_bytes_tensor(b_tensors)
 
         image_masks = []
         image_boxes = []
         image_scores = []
         image_labels = []
-        for inp in vision_inputs:
-            # frame = cv2.imdecode(np.frombuffer(enc, np.uint8), cv2.IMREAD_COLOR)
-            frame = np.array(inp.image.convert("RGB"), dtype=np.float32)
+        for enc in input_tensors:
+            frame = cv2.imdecode(np.frombuffer(enc, np.uint8), cv2.IMREAD_COLOR)
             im = letterbox(frame, self.image_size, stride=self.model.stride)[0]
             im = im.transpose((2, 0, 1))[::-1]  # HWC to CHW, BGR to RGB
             im = np.ascontiguousarray(im)  # contiguous
@@ -118,6 +157,7 @@ class StomataYolov7:
             )
 
             det_masks = []
+            labels = []
             for i, det in enumerate(pred):  # per image
                 if len(det):  # per detection
                     masks = process_mask(
@@ -134,11 +174,12 @@ class StomataYolov7:
                     ).round()
 
                     det_masks.extend(masks)
+                    labels.extend([self.label[int(d)] for d in det[:, 5]])
 
             image_masks.append(det_masks)
             image_boxes.append(pred[0][:, :4])
             image_scores.append(pred[0][:, 4])
-            image_labels.append(["stomata"] * len(pred[0]))
+            image_labels.append(labels)
 
         # og post process
         rs_boxes = []
@@ -157,8 +198,87 @@ class StomataYolov7:
             rs_scores.append(o_scores)
             rs_rles.append(o_rles)
 
-        return construct_task_instance_segmentation_output(
-            request, rs_rles, rs_labels, rs_scores, rs_boxes
+        max_boxes = max([len(i) for i in rs_boxes])
+        for b in rs_boxes:
+            for _ in range(max_boxes - len(b)):
+                b.append([0, 0, 0, 0])
+        max_labels = max([len(i) for i in rs_labels])
+        for lb in rs_labels:
+            for _ in range(max_labels - len(lb)):
+                lb.append("")
+        max_scores = max([len(i) for i in rs_scores])
+        for sc in rs_scores:
+            for _ in range(max_scores - len(sc)):
+                sc.append(0)
+
+        max_rles = max([len(i) for i in rs_rles])
+        for rles in rs_rles:
+            for _ in range(max_rles - len(rles)):
+                rles.append("")
+
+        resp_outputs = []
+        resp_raw_outputs = []
+
+        # rles
+        resp_outputs.append(
+            Metadata(
+                name="rles",
+                shape=[len(input_tensors), len(rs_rles[0])],
+                datatype=str(DataType.TYPE_STRING),
+            )
+        )
+        rles_out = []
+        for r in rs_rles:
+            rles_out.extend(r)
+        if len(rles_out) != 0:
+            rles_out = [bytes(f"{rles_out[i]}", "utf-8") for i in range(len(rles_out))]
+            resp_raw_outputs.append(serialize_byte_tensor(np.asarray(rles_out)))
+        else:
+            resp_raw_outputs.append(b"")
+
+        # boxes
+        resp_outputs.append(
+            Metadata(
+                name="boxes",
+                shape=[len(input_tensors), len(rs_boxes[0]), 4],
+                datatype=str(DataType.TYPE_FP32),
+            )
+        )
+        resp_raw_outputs.append(np.asarray(rs_boxes).astype(np.float32).tobytes())
+
+        # labels
+        resp_outputs.append(
+            Metadata(
+                name="labels",
+                shape=[len(input_tensors), len(rs_labels[0])],
+                datatype=str(DataType.TYPE_STRING),
+            )
+        )
+        labels_out = []
+        for r in rs_labels:
+            labels_out.extend(r)
+        if len(labels_out) != 0:
+            labels_out = [
+                bytes(f"{labels_out[i]}", "utf-8") for i in range(len(labels_out))
+            ]
+            resp_raw_outputs.append(serialize_byte_tensor(np.asarray(labels_out)))
+        else:
+            resp_raw_outputs.append(b"")
+
+        # scores
+        resp_outputs.append(
+            Metadata(
+                name="scores",
+                shape=[len(input_tensors), len(rs_scores[0])],
+                datatype=str(DataType.TYPE_FP32),
+            )
+        )
+        resp_raw_outputs.append(np.asarray(rs_scores).astype(np.float32).tobytes())
+
+        return construct_infer_response(
+            req=req,
+            outputs=resp_outputs,
+            raw_outputs=resp_raw_outputs,
         )
 
 
