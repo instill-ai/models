bec99f60b13a5ff64cda97bf0b55cc18d4cd423e
HR Wu <5631010+heiruwu@users.noreply.github.com>
2024-09-10 07:51:07 +0800

    chore(sdxl): fix image output base64 encode (#21)
    Because
    
    - sdxl image output encoding format is wrong
    
    This commit
    
    - fix image output base64 encode

--- stable-diffusion-xl/v0.1.0/model.py
+++ stable-diffusion-xl/v0.1.0/model.py
@@ -4,6 +4,7 @@ import random
 import numpy as np
 import torch
 
+from io import BytesIO
 from diffusers import DiffusionPipeline
 from instill.helpers.ray_config import instill_deployment, InstillDeployable
 from instill.helpers import (
@@ -16,7 +17,10 @@ from instill.helpers import (
 class SdXL:
     def __init__(self):
         self.base = DiffusionPipeline.from_pretrained(
-            "stable-diffusion-xl-base-1.0", torch_dtype=torch.float16, variant="fp16", use_safetensors=True
+            "stable-diffusion-xl-base-1.0",
+            torch_dtype=torch.float16,
+            variant="fp16",
+            use_safetensors=True,
         ).to("cuda")
         self.refiner = DiffusionPipeline.from_pretrained(
             "stable-diffusion-xl-refiner-1.0",
@@ -27,38 +31,41 @@ class SdXL:
             use_safetensors=True,
         ).to("cuda")
 
-
     async def __call__(self, request):
         inputs = await parse_task_text_to_image_input(request=request)
 
         n_steps = 40
-        high_noise_frac = 0.8 
+        high_noise_frac = 0.8
 
         finish_reasons = []
         images = []
         for inp in inputs:
-            random.seed(inp.seed)
-            np.random.seed(inp.seed)
+            if inp.seed > 0:
+                random.seed(inp.seed)
+                np.random.seed(inp.seed)
 
             base_images = self.base(
                 prompt=inp.prompt,
                 num_inference_steps=n_steps,
-                denoising_end=high_noise_frac,
+                # denoising_end=high_noise_frac,
+                guidance_scale=7.5,
                 output_type="latent",
             ).images
 
             generated_images = self.refiner(
                 prompt=inp.prompt,
                 num_inference_steps=n_steps,
-                denoising_end=high_noise_frac,
+                # denoising_end=high_noise_frac,
+                guidance_scale=7.5,
                 image=base_images,
             ).images
 
             imgs = []
             finishes = []
             for img in generated_images:
-                image_bytes = img.tobytes()
-                imgs.append(base64.b64encode(image_bytes).decode())
+                buff = BytesIO()
+                img.save(buff, format="JPEG", keep_rgb=True, quality=95)
+                imgs.append(base64.b64encode(buff.getvalue()).decode())
                 finishes.append("success")
 
             images.append(imgs)
